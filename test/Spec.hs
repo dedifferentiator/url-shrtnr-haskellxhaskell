62840bd285bfdcade3bfd0e67fd3f0e36e4bf59f
Arjaz <arjaz@protonmail.com>
2021-02-23 23:55:29 +0200

    Add a test that always fails

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,8 +10,5 @@ import Control.Exception (evaluate)
 main :: IO ()
 main = hspec $ do
   describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
-
-    it "throws an exception if used with an empty list" $ do
-      evaluate (head []) `shouldThrow` anyException
+    it "this test always fails" $ do
+      head [23 ..] `shouldBe` (0 :: Int)
